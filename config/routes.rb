7f8bf6371040307150198f1881e667a6f70215d6
Daihuku2 <70841018+Daihuku2@users.noreply.github.com>
2020-09-07 07:16:17 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  get 'posts', to: 'posts#index'
+end
+
+# クライアント側
+# クライアントがパス(url)を送信
+# 例：http://localhost:3000/posts
+
+# --------------------------------
+# ↓
+# --------------------------------
+# サーバー側
+
+# ルーティング（どのパスが来たら、どのコントローラーのどのアクションを動かすかを定義）
+# 例：get 'posts', to: 'posts#index'（postsを受け取った場合、posts#indexを動かす）
+# ↓
+# コントローラー（posts#index）
+# index = 一覧表示
+# ↓
+# ビュー
+# posts ディレクトリー
+# index ファイル
+# ↓
+# ブラウザ
